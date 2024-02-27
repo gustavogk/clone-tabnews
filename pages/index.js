793a8b74b73a382776d1a6f0b8dbcd542ce08990
Gustavo Assunção <gust.krv@gmail.com>
2024-02-27 22:08:10 +0000

    add frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 function Home (){
-    return <h1>Teste</h1>
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }} > 
+            <div style={{ maxWidth: '600px', textAlign: 'center', padding: '20px', border: '2px solid #ccc', borderRadius: '10px', marginBottom: '20px', fontFamily: 'Kode Mono, monospace', fontSize: '20px' }}>
+                <p>"Se estiver se sentindo desmotivado, ou achando que não é bom o suficiente, incendeie 🔥 o seu coração💓. Enxugue as lágrimas e siga em frente. Quando se entristecer ou se acovardar, lembre-se: o fluxo do tempo nunca para, e ele não vai te esperar enquanto você se afoga em tristeza."</p>
+            </div>
+            <div style={{ position: 'relative', width: '470px', height: '670px' }}>
+                <Image src="/images/rengoku.png" layout="fill" objectFit="contain" />
+            </div>
+        </div>
+    );
 }
 
 export default Home;
