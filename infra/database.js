5c250a6d97326231d8347e2c8078d245f83137ef
Gustavo Assunção <gust.krv@gmail.com>
2024-04-05 23:08:55 +0000

    add `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,9 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
@@ -28,10 +20,24 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
+  return process.env.NODE_ENV === "production" ? true : false;
+}
 
-  return process.env.NODE_ENV === "development" ? false : true;
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
 }
 
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
